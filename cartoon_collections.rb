582bc424feefad4fb98383f56676289ddacb77f3
Andres Carvajal <jc1993box@hotmail.com>
2019-07-30 18:50:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,9 @@ def long_planeteer_calls (array)
   end
 end
 
-
 def find_the_cheese (array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-    array.include? (item)
+    cheese_types.include? (item)
  end
  end
-cheese_types = ["cheddar", "gouda", "camembert"]
-  find_the_cheese (cheese_types)
